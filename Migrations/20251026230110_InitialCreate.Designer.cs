aa5a43c2308a229c5b827d1ea9c73d7e6d912594
Harun <karacalar500@gmail.com>
2025-10-27 02:33:26 +0300

    Update EF Core migration and image reference
    Removed old migration files and added updated versions for
    initial database schema setup in the survey application.
    Changed image source in `Index.cshtml` to use a local file
    (`indir.jpg`) and added the corresponding image to the project.

--- Migrations/20251026230110_InitialCreate.Designer.cs
+++ Migrations/20251026230110_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using SurveyApp.Data;
 namespace SurveyApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251026175555_InitialCreate")]
+    [Migration("20251026230110_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
